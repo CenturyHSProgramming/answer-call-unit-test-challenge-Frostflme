0e4ff9c0dabac9d686b9986615ae4497186fba6b
Snowspace <rjshiosaki@icloud.com>
2023-09-27 09:46:51 -0700

    made everything, still needs testing

--- answerCall.py
+++ answerCall.py
@@ -5,10 +5,52 @@
 # or visit (https://github.com/HundredVisionsGuy/answerCall)
 
 # Write function defintion: answerCall()
+def answerCall(caller_codes: str, sameAreaCode: bool, cur_time: str) -> bool:
+    validTime = checkValidTime(cur_time)
+    temp = checkCallerCode(caller_codes)
+    isTelemarketer = temp[1]
+    isFriendRelative = temp[0]
+
+    # The nest of if statements
+    if validTime:
+        if isTelemarketer:
+            return False
+        elif isFriendRelative:
+            return True
+        else:
+            if sameAreaCode:
+                return True
+            else:
+                return False
+    else: return False
 
 # Make sure it returns a value
 
+def checkValidTime(time: str) -> bool:
+    """This function checks to see if the time is an acceptable time to respond to the call. If it's between 22:00 and 7:00 exclusive, then don't respond."""
+    temp = time.split(":")
+    hours, minutes = (int(temp[0]), int(temp[1]))
+    if hours > 22 or hours < 7:
+        return False
+    elif hours == 22:
+        if minutes != 0: return False
+        else: return True
+    else: return True
+
+def checkCallerCode(caller: str) -> tuple:
+    """This function checks the caller to see who's calling. It returns two booleans in a tuple, the first one representing if it's a friend or relative, and the second one representing if it's a telemarketer. Both shouln't be true at the same time."""
+    if caller == "U":
+        return (False, False)
+    elif caller == "T":
+        return (False, True)
+    else:
+        return (True, True)
+
 if __name__ == '__main__':
-    # Call the function in here if you want to test it
-    # Make sure it's indented
-    pass # remove or comment out this line if you wish to test the function
+    print(checkValidTime("11:27"))
+    print(checkValidTime("20:23"))
+    print(checkValidTime("00:00"))
+    print(checkValidTime("22:00"))
+    print(checkValidTime("22:01"))
+    print(checkValidTime("7:00"))
+    print(checkValidTime("6:59"))
